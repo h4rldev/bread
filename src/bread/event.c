24502af59c002b85ab2fe84ec64bb254d2ef05ee
h4rl <h4rldev@proton.me>
2026-06-15 00:03:17 +0200

    fix: remove firing event debug log

--- src/bread/event.c
+++ src/bread/event.c
@@ -1,7 +1,6 @@
 #include <bread/event.h>
 #include <bread/types.h>
 #include <bread/window.h>
-#include <stdio.h>
 
 void bread_window_set_event_callback(bread_window_t *window,
                                      bread_event_callback_t callback,
@@ -11,8 +10,6 @@ void bread_window_set_event_callback(bread_window_t *window,
 }
 
 void fire_event(bread_window_t *window, bread_event_t *event) {
-  fprintf(stderr, "Firing event\n");
-
   if (window->event_callback)
     window->event_callback(event, window->event_userdata);
 }
